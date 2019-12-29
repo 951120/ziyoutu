8dd7f906d19b2971f4d7ecf6862c89056d3846b5
王权（家） <justwe9517@foxmail.com>
2019-12-29 19:44:29 +0800

    update

--- site/www.lmmpic.com.xz.rosi/index.js
+++ site/www.lmmpic.com.xz.rosi/index.js
@@ -4,8 +4,8 @@ try {
     var articleTitle = "";
     var articleDate = "";
     var articleContent = "";
-    var imageRoot = document.location.href;
-    var articleTitle = $(".entry-title").html();
+    var imageRoot = document.location.href.replace(".htm", "");
+    var articleTitle = $(".des h1").html();
 
     var bodyElement = document.getElementById("45276-content-container");
     var html = "";
@@ -14,15 +14,39 @@ try {
     bodyElement.innerHTML = html;
 
     // 取得页面所有图片
-    $.get(imageRoot + '?viewall=true', {}, function (resp) {
-        $(resp).find('.single-content img').each(function () {
-            var img = document.createElement('img');
-            img.src = this.src;
-            img.setAttribute('referrerpolicy', 'no-referrer');
-            $('#45276-content-container .content').append(img);
-        });
-        optimizer();
+    $('.pp img').each(function () {
+        var img = document.createElement('img');
+        img.src = this.src;
+        $('.content').append(img);
     });
+
+    // 取得文章一共几个页面
+    try {
+        var pageResult = $(".page-show a:last-child").prev().html().split('..');
+        if (pageResult.length < 2) {
+            var pageMax = parseInt(pageResult[0]);
+        } else {
+            var pageMax = parseInt(pageResult[1]);
+        }
+    } catch (e) {
+        var pageMax = 0;
+    }
+
+    for (var i = 2; i <= pageMax; i++) {
+        var currentUrl = imageRoot + i + ".htm";
+        $.get(currentUrl, {}, function (resp) {
+            $(resp).find('img').each(function () {
+                if (/\/pic[\d]+/ig.test(this.src)) {
+                    var img = document.createElement('img');
+                    img.src = this.src;
+                    $('.content').append(img);
+                }
+            });
+            optimizer();
+        });
+    }
+
+    optimizer();
 
 } catch (e) {
     news.onReceivedError();
